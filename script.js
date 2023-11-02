abdfc6b4e49d6cd5cb540f70f058af38900b33f6
Milla <143414504+millaplompp@users.noreply.github.com>
2023-11-02 20:52:45 -0300

    updating

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
-    console.log(data);
-
     document.querySelector('.city').innerHTML = data.name;
     document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
@@ -39,6 +37,8 @@ async function checkWeather(city){
     else if(data.weather[0].main == 'Drizzle'){
         weatherIcon.src = "weather-app-img/images/drizzle.png"
     }
+
+    document.querySelector('.weather').style.display = 'block'
 }
 
 searchBtn.addEventListener('click', () => {
